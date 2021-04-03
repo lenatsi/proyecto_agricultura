e34138997b31cd9aa8654bfd944369fdae338a55
lenatsi <tsitukhinaalena@gmail.com>
2021-04-03 19:05:36 +0200

    Creado componente menú y página principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopMenuComponent } from './top-menu/top-menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [		
+    AppComponent,
+      TopMenuComponent,
+      HomePageComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
